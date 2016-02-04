669b1505aa8c395175a5932f772fa36c54f70f5a
terry3 <terryr3rd@yeah.net>
2016-02-04 14:28:31 +0800

    change theme to almost-monokai, add yaml mode.

---
+++ t-yaml.el
@@ -0,0 +1,9 @@
+;; yaml mode
+(require 'yaml-mode)
+(add-to-list 'auto-mode-alist '("\\.yml$" . yaml-mode))
+
+(add-hook 'yaml-mode-hook
+      '(lambda ()
+         (define-key yaml-mode-map "\C-m" 'newline-and-indent)))
+
+(provide 't-yaml)
