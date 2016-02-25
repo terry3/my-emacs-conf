db83eb629a3dd123019deeb8abef859845c59705
terry3 <terryr3rd@yeah.net>
2016-02-25 11:06:24 +0800

    remove useless code.(magit)

--- t-lisp/t-git.el
+++ t-lisp/t-git.el
@@ -1,6 +1,5 @@
 ;; git & git front end config
 ;; bind keys because use so often
-(setq magit-last-seen-setup-instructions "1.4.0")
 (el-get-bundle 'magit)
 (global-set-key (kbd "C-x g") 'magit-status)
 (global-set-key (kbd "C-x M-g") 'magit-dispatch-popup)
