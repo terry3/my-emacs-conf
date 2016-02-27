8f4a3e495a436bbb88a984a2d8b77c99f39ae9c0
terry3 <tfguo369@gmail.com>
2016-02-27 17:29:28 +0800

    remove useless code.

--- init.el
+++ init.el
@@ -49,19 +49,4 @@
 ;; sync recipes now
 (el-get 'sync)
 (provide 't-init)
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(custom-safe-themes
-   (quote
-    ("a27c00821ccfd5a78b01e4f35dc056706dd9ede09a8b90c6955ae6a390eb1c1e" default)))
- '(scroll-bar-mode nil)
- '(tool-bar-mode nil))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
+
