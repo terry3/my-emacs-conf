9cca781541f22a4e2d069e5978d71fbc1c3ad3c0
terry3 <tfguo369@gmail.com>
2016-02-28 10:15:02 +0800

    update backup fn.

--- t-lisp/t-backup.el
+++ t-lisp/t-backup.el
@@ -1,7 +1,7 @@
 ;; set backup directory
 (setq backup-by-copying t
       backup-directory-alist
-      '(("." . "/Users/tengfeiguo/.emacs_backup")) ; Remember to modify this when change os.
+      '(("." . "~/.emacs_backup")) ; Remember to modify this when change os.
       delete-old-versions t ; delete old version auto
       kept-new-versions 6 ; keep 6 backup
       kept-old-versions 2
