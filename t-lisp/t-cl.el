ff8e225f177b1c3d119e132eaeb4dce70f129a55
terry3 <terryr3rd@yeah.net>
2016-03-03 17:21:38 +0800

    update smartparens config for coding common-lisp.

--- t-lisp/t-cl.el
+++ t-lisp/t-cl.el
@@ -1,3 +1,17 @@
 (el-get-bundle slime)
 (setq inferior-lisp-program "/usr/local/bin/sbcl")
+
+(sp-with-modes sp--lisp-modes
+  ;; disable ', it's the quote character!
+  (sp-local-pair "'" nil :actions nil)
+  ;; also only use the pseudo-quote inside strings where it serve as
+  ;; hyperlink.
+  (sp-local-pair "`" "'" :when '(sp-in-string-p sp-in-comment-p))
+  (sp-local-pair "`" nil
+                 :skip-match (lambda (ms mb me)
+                               (cond
+                                ((equal ms "'")
+                                 (or (sp--org-skip-markup ms mb me)
+                                     (not (sp-point-in-string-or-comment))))
+                                (t (not (sp-point-in-string-or-comment)))))))
 (provide 't-cl)
