577383d19e8f8aa29781c71be25337a3e3456fc1
Tengfei Guo <terryr3rd@yeah.net>
2017-01-11 15:14:53 +0800

    Add revert-buffer command.

--- t-lisp/t-edit.el
+++ t-lisp/t-edit.el
@@ -51,5 +51,19 @@
 ;; remove useless key binding
 (global-unset-key (kbd "M-c"))
 
+;; revert buffer no confirm
+(global-set-key
+ (kbd "<f5>")
+ (lambda (&optional force-reverting)
+   "Interactive call to revert-buffer. Ignoring the auto-save
+ file and not requesting for confirmation. When the current buffer
+ is modified, the command refuses to revert it, unless you specify
+ the optional argument: force-reverting to true."
+   (interactive "P")
+   ;;(message "force-reverting value is %s" force-reverting)
+   (if (or force-reverting (not (buffer-modified-p)))
+       (revert-buffer :ignore-auto :noconfirm)
+     (error "The buffer has been modified"))))
+
 (provide 't-edit)
 ;;; t-edit ends here
