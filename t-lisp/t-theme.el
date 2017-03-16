59f0bc39217141df0dafbf1cc6506a3218af8e43
Tengfei Guo <terryr3rd@yeah.net>
2017-03-16 16:54:21 +0800

    Update theme to tangotango.

--- t-lisp/t-theme.el
+++ t-lisp/t-theme.el
@@ -4,16 +4,16 @@
 ;; (require 'zenburn-theme)
 
 ;; tangotango
-;; (el-get-bundle color-theme-tangotango
-;;   (color-theme-tangotango))
+(el-get-bundle color-theme-tangotango
+  (color-theme-tangotango))
 
 ;; (add-to-list 'custom-theme-load-path "~/.emacs.d/misc/")
 
 ;; (el-get-bundle john2x/flatui-theme.el
 ;;   (require 'flatui-theme))
 
-(el-get-bundle leuven-theme
-  (load-theme 'leuven t))
+;; (el-get-bundle leuven-theme
+;;   (load-theme 'leuven t))
 
 ;; terry3/oceanic-theme
 ;; (el-get-bundle terry3/oceanic-theme
