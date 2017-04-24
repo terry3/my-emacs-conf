f2a48b36fb9efd481ae3cba804d79d74fff2c0f0
Tengfei Guo <terryr3rd@yeah.net>
2017-04-24 11:33:41 +0800

    Compatibility with nunjucks and typescript.

--- t-lisp/t-web.el
+++ t-lisp/t-web.el
@@ -18,5 +18,7 @@
 (add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.ejs?\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.wxml?\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.njk?\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.ts?\\'" . web-mode))
 
 (provide 't-web)
