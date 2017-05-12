0f53783860c5260313ef476b2c3c69c2a59d0033
Tengfei Guo <terryr3rd@yeah.net>
2017-05-12 18:53:23 +0800

    Update typescript config.

--- t-lisp/t-ts.el
+++ t-lisp/t-ts.el
@@ -22,4 +22,10 @@
 
 (add-hook 'typescript-mode-hook #'setup-tide-mode)
 
+(add-to-list 'auto-mode-alist '("\\.tsx\\'" . web-mode))
+(add-hook 'web-mode-hook
+          (lambda ()
+            (when (string-equal "tsx" (file-name-extension buffer-file-name))
+              (setup-tide-mode))))
+
 (provide 't-ts)
