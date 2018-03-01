0883d27b00894e18abc74ff778e918ebf1ab5144
Tengfei Guo <guotengfei02@meituan.com>
2018-03-01 14:17:34 +0800

    Update theme, from dracula to doom.

--- init.el
+++ init.el
@@ -36,3 +36,17 @@
 
 (el-get 'sync)
 (provide 't-init)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages (quote (queue)))
+ '(scroll-bar-mode nil)
+ '(tool-bar-mode nil))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
--- t-lisp/t-theme.el
+++ t-lisp/t-theme.el
@@ -4,11 +4,31 @@
 ;; (require 'zenburn-theme)
 
 ;; tangotango
-;; (el-get-bundle color-theme-tangotango
-;;   (color-theme-tangotango))
+;; (el-get-bundle juba/color-theme-tangotango
+;;  (load-theme 'tangotango t))
 
 (add-to-list 'custom-theme-load-path "~/.emacs.d/misc/")
-(load-theme 'dracula t)
+(add-to-list 'load-path "~/.emacs.d/misc/emacs-doom-themes/")
+
+(require 'doom-themes)
+
+;; Global settings (defaults)
+(setq doom-themes-enable-bold t    ; if nil, bold is universally disabled
+      doom-themes-enable-italic t) ; if nil, italics is universally disabled
+
+;; Load the theme (doom-one, doom-molokai, etc); keep in mind that each theme
+;; may have their own settings.
+(load-theme 'doom-one t)
+
+;; Enable flashing mode-line on errors
+(doom-themes-visual-bell-config)
+
+;; Enable custom neotree theme
+(doom-themes-neotree-config)  ; all-the-icons fonts must be installed!
+
+;; Corrects (and improves) org-mode's native fontification.
+(doom-themes-org-config)
+;; (load-theme 'dracula t)
 ;; (el-get-bundle leuven-theme
 ;;   (load-theme 'leuven t))
 
