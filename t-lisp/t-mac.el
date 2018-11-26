aa159687748f229945574fb8a94c3a2d6824452f
Tengfei Guo <guotengfei02@maoyan.com>
2018-11-26 14:42:09 +0800

    update theme.

--- t-lisp/t-mac.el
+++ t-lisp/t-mac.el
@@ -7,7 +7,7 @@
       (setq mac-option-modifier nil)
       (setq visible-bell nil)      ;; workaround the visible-bell in OSX EI
       (setq ring-bell-function 'ignore)
-      (set-default-font "Monaco 14")
+      (set-default-font "Monaco 13")
       (setq-default line-spacing 5)))
 
 (provide 't-mac)
