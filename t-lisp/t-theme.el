92de5a462329e4dc7aaf3d3f0efd1bbe7b6657cf
Tengfei Guo <guotengfei02@maoyan.com>
2019-03-18 17:48:51 +0800

    feat(emacs): update theme config.

--- t-lisp/t-theme.el
+++ t-lisp/t-theme.el
@@ -1,48 +1,40 @@
-;; With initialization code
-;; (el-get-bundle zenburn-theme
-;;   :url "https://raw.githubusercontent.com/bbatsov/zenburn-emacs/master/zenburn-theme.el")
-;; (require 'zenburn-theme)
-
-;; tangotango
-;; (el-get-bundle juba/color-theme-tangotango
-;;  (load-theme 'tangotango t))
-
-(add-to-list 'custom-theme-load-path "~/.emacs.d/misc/")
-;; (add-to-list 'load-path "~/.emacs.d/misc/emacs-doom-themes/")
-
-(el-get-bundle 'doom-themes)
+;; terry3's emacs theme config
+(el-get-bundle 'doom-themes
+  (progn
+    ;; (load-theme 'doom-one t)
+    ;; (load-theme 'doom-vibrant t)
+    ;; (load-theme 'doom-Iosvkem t)
+    ;; (load-theme 'doom-challenger-deep t)
+    ;; (load-theme 'doom-city-lights t)
+    (load-theme 'doom-dracula t)
+    ;; (load-theme 'doom-molokai t)
+    ;; (load-theme 'doom-nord-light t)
+    ;; (load-theme 'doom-nord t)
+    ;; (load-theme 'doom-nova t)
+    ;; (load-theme 'doom-one-light t)
+    ;; (load-theme 'doom-one t)
+    ;; (load-theme 'doom-opera-light t)
+    ;; (load-theme 'doom-opera t)
+    ;; (load-theme 'doom-peacock t)
+    ;; (load-theme 'doom-solarized-light t)
+    ;; (load-theme 'doom-sourcerer t)
+    ;; (load-theme 'doom-spacegrey t)
+    ;; (load-theme 'doom-tomorrow-day t)
+    ;; (load-theme 'doom-tomorrow-night t)
+    ;; (load-theme 'doom-vibrant t)
+    ))
 
 ;; Global settings (defaults)
 (setq doom-themes-enable-bold t    ; if nil, bold is universally disabled
       doom-themes-enable-italic t) ; if nil, italics is universally disabled
 
-;; Load the theme (doom-one, doom-molokai, etc); keep in mind that each theme
-;; may have their own settings.
-;; (load-theme 'doom-one t)
-(load-theme 'doom-vibrant t)
-;; (load-theme 'doom-molokai t)
-;; (load-theme 'doom-nova t)
-;; (load-theme 'doom-one-light t)
-;; (load-theme 'doom-peacock t)
-;; (load-theme 'doom-tomorrow-night t)
-;; (load-theme 'doom-spacegrey t)
-
 ;; Enable flashing mode-line on errors
-;; (doom-themes-visual-bell-config)
+(doom-themes-visual-bell-config)
 
 ;; Enable custom neotree theme
-;; (doom-themes-neotree-config)  ; all-the-icons fonts must be installed!
+(doom-themes-neotree-config)  ; all-the-icons fonts must be installed!
 
 ;; Corrects (and improves) org-mode's native fontification.
-;; (doom-themes-org-config)
-;; (load-theme 'dracula t)
-;; (el-get-bundle leuven-theme
-;;   (load-theme 'leuven t))
-
-;; terry3/oceanic-theme
-;; (el-get-bundle terry3/oceanic-theme
-;;   (progn
-;;     (require 'oceanic-theme)
-;;     (load-theme 'oceanic t)))
+(doom-themes-org-config)
 
 (provide 't-theme)
